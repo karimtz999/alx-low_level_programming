17aa59a5ade7fb1fc634982e9e09feffc41e71a2
karimtz999 <farkarim36@gmail.com>
2023-09-10 09:18:08 -0700

    0x01-variables_if_else_while

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -34,26 +34,3 @@ int main(void)
 	return (0);
 
 }
-
-
-2-print_alphabet.c
-
-#include <stdio.h>
-
-/**
- * main - Prints the alphabet.
- *
- * Return: Always 0 (Success)
- */
-int main(void)
-{
-	char alp[26] = "abcdefghijklmnopqrstuvwxyz";
-	int i;
-
-	for (i = 0; i < 26; i++)
-	{
-		putchar(alp[i]);
-	}
-	putchar('\n');
-	return (0);
-}
