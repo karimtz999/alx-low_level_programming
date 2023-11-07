6d54d94b6003cb3d0b5bc220cc65aa124e6f2725
karimtz999 <farkarim36@gmail.com>
2023-11-07 12:47:46 -0800

    0x0D-preprocessor

---
+++ 0x0D-preprocessor/4-main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "4-main.c"
+
+/**
+ * main - check the code
+ *
+ * Return: always 0.
+ */
+int main(void)
+{
+	int s;
+
+	s = SUM(98, 1024);
+	printf("%d\n", s);
+	return (0);
+}
